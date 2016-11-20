87f573755197967a1df7d3298a2c7aaf879d46d3
Florian Oefinger <florian@serlo.org>
2016-11-20 14:35:04 +0100

    Remove App component

--- components/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-
-import App from './App'
-
-it('App renders correctly', () => {
-  expect(renderer.create(<App />)).toMatchSnapshot()
-})
