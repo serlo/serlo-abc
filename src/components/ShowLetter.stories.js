b10054f37196262f56bea1e228026450419f83bf
Jonas Keinholz <jonas@keinholz.com>
2017-03-30 03:03:05 +0200

    ShowLetter: remove wrongly played repeat sound

--- src/components/ShowLetter.stories.js
+++ src/components/ShowLetter.stories.js
@@ -3,10 +3,10 @@ import { storiesOf } from '@kadira/react-native-storybook';
 
 import ShowLetter from './ShowLetter';
 
-storiesOf('components/ShowLetter', module).add('default', () => (
-  <ShowLetter
-    letter="A"
-    sound={require('../assets/sounds/a.mp3')}
-    isRepeat={true}
-  />
-));
+storiesOf('components/ShowLetter', module)
+  .add('A (without repeat)', () => (
+    <ShowLetter letter="A" sound={require('../assets/sounds/a.mp3')} />
+  ))
+  .add('B (with repeat)', () => (
+    <ShowLetter letter="B" sound={require('../assets/sounds/b.mp3')} isRepeat />
+  ));
