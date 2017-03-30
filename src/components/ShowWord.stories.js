47eb42e9ec69f0b99ac19fdaf276d8609793bb05
Jonas Keinholz <jonas@keinholz.com>
2017-03-30 03:07:04 +0200

    storybook: naming, additional example

--- src/components/ShowWord.stories.js
+++ src/components/ShowWord.stories.js
@@ -3,14 +3,26 @@ import { storiesOf } from '@kadira/react-native-storybook';
 
 import ShowWord from './ShowWord';
 
-storiesOf('components/ShowWord', module).add('default', () => (
-  <ShowWord
-    image={require('../assets/images/ananas.jpg')}
-    sounds={[
-      require('../assets/sounds/ananas_long.mp3'),
-      require('../assets/sounds/ananas_short.mp3')
-    ]}
-    text="Ananas"
-    letter="A"
-  />
-));
+storiesOf('components/ShowWord', module)
+  .add('Ananas', () => (
+    <ShowWord
+      image={require('../assets/images/ananas.jpg')}
+      sounds={[
+        require('../assets/sounds/ananas_long.mp3'),
+        require('../assets/sounds/ananas_short.mp3')
+      ]}
+      text="Ananas"
+      letter="A"
+    />
+  ))
+  .add('Affe', () => (
+    <ShowWord
+      image={require('../assets/images/affe.jpg')}
+      sounds={[
+        require('../assets/sounds/affe_long.mp3'),
+        require('../assets/sounds/affe_short.mp3')
+      ]}
+      text="Affe"
+      letter="A"
+    />
+  ));
