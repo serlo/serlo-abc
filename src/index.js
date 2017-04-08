65728b7949dffb6ccea9de755c16b36e81f67325
Jonas Keinholz <jonas@keinholz.com>
2017-04-08 23:31:48 +0200

    all: remove redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import { Provider, connect } from 'react-redux';
-
-import { selectView } from './reducers';
-import createStore from './store';
-import { getTemplate } from './templates/Template';
-
-const Container = ({ view }) => {
-  const T = getTemplate(view.template);
-  return <T {...view} />;
-};
-
-const mapStateToProps = state => ({
-  view: selectView(state)
-});
-
-const ConnectedContainer = connect(mapStateToProps)(Container);
-
-const store = createStore();
+import { Text } from 'react-native';
 
 const App = () => (
-  <Provider store={store}>
-    <ConnectedContainer />
-  </Provider>
+  <Text>Welcome to Serlo ABC</Text>
 );
 
 export default App;
