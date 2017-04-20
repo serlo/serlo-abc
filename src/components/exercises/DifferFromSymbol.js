f85b853a46241fc9d3433323d02f3f27cd86eae3
metzgaria <mele-metzger@web.de>
2017-04-20 10:27:32 +0200

    screen: Splash (#94)
    * screen: Splash
    * exercises: DifferFromSymbol

---
+++ src/components/exercises/DifferFromSymbol.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View } from 'react-native';
+import { PRIMARY } from '../../styles/colors';
+import { RoundTextButton, RoundText } from '../Components';
+
+const DifferFromSymbol = ({ symbols }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: PRIMARY,
+        alignItems: 'center'
+      }}
+    >
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: 300,
+          marginTop: 50
+        }}
+      >
+        <RoundTextButton style={{ margin: 5 }} text={symbols[0]} size={80} />
+        <RoundTextButton style={{ margin: 5 }} text={symbols[1]} size={80} />
+        <RoundTextButton style={{ margin: 5 }} text={symbols[2]} size={80} />
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: 300
+        }}
+      >
+        <RoundTextButton style={{ margin: 5 }} text={symbols[3]} size={80} />
+        <RoundTextButton style={{ margin: 5 }} text={symbols[4]} size={80} />
+      </View>
+    </View>
+  );
+};
+
+export default DifferFromSymbol;
