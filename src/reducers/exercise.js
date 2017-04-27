9b0fa0a602b281988d101c8fc746ff3fd9de82f5
Igor Zamyslov <igor.zamyslov@cognostics.de>
2017-04-27 15:16:25 +0200

    Basic exercise logic implemented: Added some logic to one of the exercises (DifferFromSymbol) to be able to select answer and change button colour based on the selected answer and submission status (success/fail). Added actions to exercise-reducer to: select an answer, submit an answer for current exercise. Now instead of generating separate queue of exercises we preserve a course tree.

--- src/reducers/exercise.js
+++ src/reducers/exercise.js
@@ -1,43 +1,73 @@
 import * as types from '../constants/actionTypes';
-import { AsyncStorage, Alert } from 'react-native';
 
 const course = require('../assets/courses/course.json');
 
-// make a "timeline" for exercises
-const exercises = [];
+// add additional information to exercises
 for (let i = 0; i < course.sections.length; i++) {
   const section = course.sections[i];
   for (let j = 0; j < section.chapters.length; j++) {
     const chapter = section.chapters[j];
     for (let k = 0; k < chapter.exercises.length; k++) {
       const exercise = chapter.exercises[k];
-      exercise.complete = false; // later take this info from storage
-      exercises.push(exercise);
+      exercise.complete = false;
+      exercise.success = false;
+      exercise.section = i;
+      exercise.chapter = j;
+      exercise.originalIndex = null;
+      // if an exercise was appended due to fail - originalIndex
+      // will contain index of original exercise
     }
   }
 }
 
 const initialState = {
   course,
-  exercises,
-  currentExercise: 0,
+  currentExercise: { section: 0, chapter: 0, exercise: 0 },
+  currentAnswer: null,
 };
 
 export default exercise = (state = initialState, action = {}) => {
+  const section = state.course.sections[state.currentExercise.section];
+  const chapter = section.chapters[state.currentExercise.chapter];
+  const exercise = chapter.exercises[state.currentExercise.exercise];
+
   switch (action.type) {
-    case types.MARK_CURRENT_EXERCISE_COMPLETE:
-      state.exercises[currentExercise].complete = true;
-      if (state.currentExercise < exercises.length - 1) {
-        state.currentExercise += 1;
+    case types.SUBMIT_EXERCISE:
+    // QUESTION: should it be possible to resubmit the same exercise?
+      exercise.complete = true;
+
+      // TODO: provide correct answer in some unified form
+      if (state.currentAnswer === exercise.props.correctIndex) {
+        exercise.success = true;
+      } else {
+        exercise.success = false;
+        if (exercise.originalIndex === null) {
+          const repetitionExercise = { ...exercise };
+          repetitionExercise.originalIndex = state.currentExercise.exercise;
+          chapter.exercises.push(repetitionExercise);
+        }
       }
       return { ...state };
       break;
+    case types.SELECT_EXERCISE_ANSWER:
+      exercise.complete = false;
+      exercise.success = false;
+      state.currentAnswer = action.answer;
+      return { ...state }
+      break;
     case types.CHANGE_EXERCISE:
-      return { ...state, currentExercise: action.index };
+      state.currentExercise = {
+        section: action.section,
+        chapter: action.chapter,
+        exercise: action.exercise,
+      }
+      state.currentAnswer = null;
+      return { ...state };
       break;
     case types.NEXT_EXERCISE:
-      if (state.currentExercise < exercises.length - 1) {
-        state.currentExercise += 1;
+      if (state.currentExercise.exercise < chapter.exercises.length - 1) {
+        state.currentExercise.exercise += 1;
+        state.currentAnswer = null;
       }
       return { ...state };
       break;
