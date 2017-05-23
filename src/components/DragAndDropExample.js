07ee533a533146c396e891713f5ae5924e84dcd4
Yiyang <yiyang.li940118@gmail.com>
2017-05-23 11:28:28 +0200

    1. Drag and Drop: different width rectangle centered in the dot 2. Sketch: get rid of some redundant codes

--- src/components/DragAndDropExample.js
+++ src/components/DragAndDropExample.js
@@ -13,32 +13,39 @@ const CIRCLE_RADIUS = 36;
 const CIRCLE_DIAMETER = 72;
 const DOT_RADIUS = 5;
 const Window = Dimensions.get('window');
+console.log('window object', Window);
+console.log('minWidth', Window.width / 4);
 const styles = StyleSheet.create({
   text: {
     marginTop: 25,
     marginLeft: 5,
     marginRight: 5,
     textAlign: 'center',
-    color: '#fff'
+    color: '#fff',
+    fontSize: 18,
+    paddingLeft: 10,
+    paddingRight: 10
   },
   dot: {
-    backgroundColor: '#1c45cc',
+    backgroundColor: '#01579B',
     width: DOT_RADIUS * 2,
     height: DOT_RADIUS * 2,
     borderRadius: DOT_RADIUS
   },
-  circle: {
-    backgroundColor: '#1abc9c',
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
-    borderRadius: CIRCLE_RADIUS
+  rectangle: {
+    backgroundColor: '#0277BD',
+    // width: CIRCLE_DIAMETER,
+    height: CIRCLE_DIAMETER
+    // minWidth: Window.width / 4,
   }
 });
+
+const sentenceArray = ['doing', 'like', 'I', 'exercises'];
 
 export default class DragAndDropExample extends React.Component {
   constructor() {
     super();
-    this.itemNo = 4;
+    this.itemNo = 6;
     this.panResponderArr = [];
     this.initialPositionArr = [];
 
@@ -59,6 +66,7 @@ export default class DragAndDropExample extends React.Component {
       }
     }
 
+    // initialize pan array
     const panArr = [];
     for (let i = 0; i < this.itemNo; i++) {
       const panObj = new Animated.ValueXY(this.initialPositionArr[i]);
@@ -67,7 +75,8 @@ export default class DragAndDropExample extends React.Component {
 
     this.state = {
       panArr,
-      dotsLayout: []
+      dotsLayout: [],
+      recsLayout: []
     };
   }
 
@@ -77,6 +86,7 @@ export default class DragAndDropExample extends React.Component {
       const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: (e, gestureState) => {
+          console.log('grant', gestureState);
           this.state.panArr[i].setOffset({
             x: this.state.panArr[i].x._value,
             y: this.state.panArr[i].y._value
@@ -92,6 +102,7 @@ export default class DragAndDropExample extends React.Component {
         ]),
         onPanResponderRelease: (e, gesture) => {
           console.log(this.state.pan, gesture);
+          console.log('recsLayout', this.state.recsLayout[i]);
           this.state.panArr[i].flattenOffset();
           const keys = Object.keys(this.state.dotsLayout);
           const springTo = {
@@ -107,14 +118,39 @@ export default class DragAndDropExample extends React.Component {
               zone.y < gesture.moveY &&
               gesture.moveY < zone.y + zone.height
             ) {
-              springTo.x = zone.x + zone.width / 2 - CIRCLE_RADIUS;
-              springTo.y = zone.y + zone.height / 2 - CIRCLE_RADIUS;
+              springTo.x =
+                zone.x + zone.width / 2 - this.state.recsLayout[i].width / 2;
+              springTo.y =
+                zone.y + zone.height / 2 - this.state.recsLayout[i].height / 2;
               break;
+            }
+          }
+          let overlappedIndex = -1;
+          for (let j = 0; j < this.state.panArr.length; j++) {
+            if (
+              parseInt(springTo.x, 10) ===
+                parseInt(this.state.panArr[j].x._value, 10) &&
+              parseInt(springTo.y, 10) ===
+                parseInt(this.state.panArr[j].y._value, 10)
+            ) {
+              console.log(j);
+              // springTo.x = this.initialPositionArr[i].x;
+              // springTo.y = this.initialPositionArr[i].y;
+              overlappedIndex = j;
             }
           }
           Animated.spring(this.state.panArr[i], {
             toValue: { ...springTo }
           }).start();
+          if (overlappedIndex !== -1) {
+            Animated.spring(this.state.panArr[overlappedIndex], {
+              toValue: {
+                x: this.initialPositionArr[overlappedIndex].x,
+                y: this.initialPositionArr[overlappedIndex].y
+              }
+            }).start();
+          }
+          console.log(this.state.panArr);
         }
       });
       this.panResponderArr.push(panResponder);
@@ -122,9 +158,17 @@ export default class DragAndDropExample extends React.Component {
   };
 
   updateDotLayout = (event, dotNumber) => {
+    console.log('updateDotLayout', event.nativeEvent);
     const dotsLayout = this.state.dotsLayout;
     dotsLayout[dotNumber] = event.nativeEvent.layout;
     this.setState({ dotsLayout });
+  };
+
+  updateRectangleLayout = (event, recIndex) => {
+    console.log('updateRecLayout', event.nativeEvent);
+    const recsLayout = this.state.recsLayout;
+    recsLayout[recIndex] = event.nativeEvent.layout;
+    this.setState({ recsLayout });
   };
 
   renderDots = n => {
@@ -158,16 +202,17 @@ export default class DragAndDropExample extends React.Component {
     );
   };
 
-  renderDraggable = n => {
+  renderDraggable = sentenceArray => {
     const draggables = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < sentenceArray.length; i++) {
       draggables.push(
-        <View style={{ position: 'absolute' }}>
+        <View key={`rectangle_${i}`} style={{ position: 'absolute' }}>
           <Animated.View
             {...this.panResponderArr[i].panHandlers}
-            style={[this.state.panArr[i].getLayout(), styles.circle]}
+            style={[this.state.panArr[i].getLayout(), styles.rectangle]}
+            onLayout={e => this.updateRectangleLayout(e, i)}
           >
-            <Text style={styles.text}>{`Drag ${i}`}</Text>
+            <Text style={styles.text}>{`${sentenceArray[i]}`}</Text>
           </Animated.View>
         </View>
       );
@@ -181,9 +226,9 @@ export default class DragAndDropExample extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        {this.renderDots(this.itemNo)}
-        {this.renderDraggable(this.itemNo)}
+      <View style={{ flex: 1, backgroundColor: '#81D4FA' }}>
+        {this.renderDots(sentenceArray.length)}
+        {this.renderDraggable(sentenceArray)}
       </View>
     );
   }
--- src/components/SvgExample.js
+++ src/components/SvgExample.js
@@ -1,84 +1,89 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, PanResponder } from 'react-native';
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  LinearGradient,
-  RadialGradient,
-  Line,
-  Path,
-  Polygon,
-  Polyline,
-  Rect,
-  Symbol,
-  Text,
-  Use,
-  Defs,
-  Stop
-} from 'react-native-svg';
+import Svg, { Circle, Line } from 'react-native-svg';
 
 export default class SvgExample extends Component {
   constructor() {
     super();
     this.state = {
-      svgCircleArray: [],
-      previousXDistance: 0,
-      previousYDistance: 0
+      // svgCircleArray: [],
+      svgLineArray: []
     };
+    this.oldCoordX = 0;
+    this.oldCoordY = 0;
   }
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
-      // Ask to be the responder:
       onStartShouldSetPanResponder: () => true,
 
       onPanResponderGrant: (evt, gestureState) => {
-        console.log('grant');
-        this.renderCircle(gestureState);
+        console.log('grant', gestureState);
+        this.oldCoordX = gestureState.x0;
+        this.oldCoordY = gestureState.y0;
       },
       onPanResponderMove: (evt, gestureState) => {
         console.log('move');
-        // console.log(gestureState);
-        // console.log(gestureState.dx);
-        // console.log(gestureState.dy);
-        // // this.renderCircle(gestureState);
-        // if((gestureState.dx - this.state.previousXDistance) > 50 || (gestureState.dy - this.state.previousYDistance) > 50) {
-        //   console.log('meet');
-        //   this.state.previousXDistance = gestureState.dx;
-        //   this.state.previousYDistance = gestureState.dy;
-        //   this.renderCircle(gestureState);
-        // }
-        const svgCircleArray = this.state.svgCircleArray;
-        const circle = (
-          <Circle
-            cx={gestureState.x0}
-            cy={gestureState.y0}
-            r="15"
-            fill="pink"
-          />
-        );
-        svgCircleArray.push(circle);
-        this.setState({ svgCircleArray });
-        console.log(this.state.svgCircleArray);
+        if (
+          Math.abs(gestureState.moveX - this.oldCoordX) > 20 ||
+          Math.abs(gestureState.moveY - this.oldCoordY) > 20
+        ) {
+          this.renderLine(gestureState);
+          this.oldCoordX = gestureState.moveX;
+          this.oldCoordY = gestureState.moveY;
+        }
       },
       onPanResponderRelease: (evt, gestureState) => {
-        console.log('release');
-        this.renderCircle(gestureState);
+        console.log('release', gestureState);
       }
     });
   }
 
-  renderCircle = gestureState => {
-    console.log('render circles');
-    const svgCircleArray = this.state.svgCircleArray;
-    const circle = (
-      <Circle cx={gestureState.x0} cy={gestureState.y0} r="15" fill="pink" />
+  renderLine = gestureState => {
+    const svgLineArray = this.state.svgLineArray;
+    const line = (
+      <Line
+        x1={this.oldCoordX}
+        y1={this.oldCoordY}
+        x2={gestureState.moveX}
+        y2={gestureState.moveY}
+        stroke="pink"
+        strokeWidth="20"
+      />
     );
-    svgCircleArray.push(circle);
-    this.setState({ svgCircleArray });
-    console.log(this.state.svgCircleArray);
+    svgLineArray.push(line);
+    this.setState({ svgLineArray });
   };
+
+  // renderCircle = (type, gestureState) => {
+  //   console.log('render circles');
+  //   console.log(gestureState);
+  //   let coordX;
+  //   let coordY;
+  //   switch (type) {
+  //     case 'grant':
+  //       coordX = gestureState.x0;
+  //       coordY = gestureState.y0;
+  //       break;
+  //     case 'release':
+  //       coordX = gestureState.moveX;
+  //       coordY = gestureState.moveY;
+  //       break;
+  //     case 'move':
+  //       coordX = gestureState.moveX;
+  //       coordY = gestureState.moveY;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   const svgCircleArray = this.state.svgCircleArray;
+  //   const circle = (
+  //     <Circle cx={coordX} cy={coordY} r="15" fill="pink" />
+  //   );
+  //   svgCircleArray.push(circle);
+  //   this.setState({ svgCircleArray });
+  //   console.log(this.state.svgCircleArray);
+  // };
 
   render() {
     return (
@@ -88,6 +93,7 @@ export default class SvgExample extends Component {
       >
         <Svg style={{ width: '100%', height: '100%' }}>
           {this.state.svgCircleArray}
+          {this.state.svgLineArray}
         </Svg>
       </View>
     );
