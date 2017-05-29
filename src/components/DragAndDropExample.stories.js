f80e7f1732de7e599ee6424e4c8001ff9b1b0226
Igor Zamyslov <igor.zamyslov@cognostics.de>
2017-05-29 14:05:09 +0200

    Changed structure a bit, removed unrelated component for now

--- src/components/DragAndDropExample.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/react-native-storybook';
-
-import DragAndDropExample from './DragAndDropExample';
-import SvgExample from './SvgExample';
-
-storiesOf('DnD & Sketch', module).add('DnD', () => <DragAndDropExample />);
-storiesOf('DnD & Sketch', module).add('Sketch', () => <SvgExample />);
---
+++ src/components/LetterSketchExample.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@kadira/react-native-storybook';
+import LetterSketchExample from './LetterSketchExample';
+
+storiesOf('Letter Sketch', module).add('Example', () => (
+  <LetterSketchExample />
+));
