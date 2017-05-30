14e884aa045ba19f94c49cee1f89a1a3d33f1cd8
Igor Zamyslov <igor.zamyslov@cognostics.de>
2017-05-30 09:26:27 +0200

    Positioned one draggable element (set coordinates) based on initial flex position

--- src/components/DragAndDropExample.js
+++ src/components/DragAndDropExample.js
@@ -21,56 +21,63 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
   dot: {
-    backgroundColor: '#1c45cc',
+    backgroundColor: 'white',
     width: DOT_RADIUS * 2,
     height: DOT_RADIUS * 2,
-    borderRadius: DOT_RADIUS,
+    borderRadius: DOT_RADIUS
   },
   circle: {
     backgroundColor: '#1abc9c',
     width: CIRCLE_RADIUS * 2,
     height: CIRCLE_RADIUS * 2,
-    borderRadius: CIRCLE_RADIUS,
+    borderRadius: CIRCLE_RADIUS
   }
 });
 
-export default class DragAndDropExample extends React.Component{
+const sentence = 'Lorem ipsum dolor sit amet';
+
+export default class DragAndDropExample extends React.Component {
   constructor() {
     super();
-    this.initialPosition = {
-      x: Window.width / 2 - CIRCLE_RADIUS,
-      y: Window.height / 2 + Window.height / 3 - CIRCLE_RADIUS,
-    };
-
     this.state = {
-      pan: new Animated.ValueXY(this.initialPosition),
-      dotsLayout: [],
+      pan: null //new Animated.ValueXY(this.initialPosition),
     };
+    this.dotsLayout = [];
+  }
 
+  initPanResponder = index => {
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderGrant: (e, gestureState) => {
         this.state.pan.setOffset({
           x: this.state.pan.x._value,
-          y: this.state.pan.y._value,
+          y: this.state.pan.y._value
         });
         this.state.pan.setValue({ x: 0, y: 0 });
       },
-      onPanResponderMove: Animated.event([null, {
-        dx: this.state.pan.x,
-        dy: this.state.pan.y,
-      }]),
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.pan.x,
+          dy: this.state.pan.y
+        }
+      ]),
       onPanResponderRelease: (e, gesture) => {
         console.log(this.state.pan, gesture);
         this.state.pan.flattenOffset();
-        const keys = Object.keys(this.state.dotsLayout);
-        const springTo = {x: this.initialPosition.x, y: this.initialPosition.y};
+        const keys = Object.keys(this.dotsLayout);
+        const springTo = {
+          x: this.initialPosition.x,
+          y: this.initialPosition.y
+        };
         for (let i = 0; i < keys.length; i++) {
           const key = keys[i];
-          const zone = this.state.dotsLayout[key];
+          const zone = this.dotsLayout[key];
           if (
-            zone.x < gesture.moveX && gesture.moveX < zone.x + zone.width &&
-            zone.y < gesture.moveY && gesture.moveY < zone.y + zone.height
+            zone.x < gesture.moveX &&
+            gesture.moveX < zone.x + zone.width &&
+            zone.y < gesture.moveY &&
+            gesture.moveY < zone.y + zone.height
           ) {
             springTo.x = zone.x + zone.width / 2 - CIRCLE_RADIUS;
             springTo.y = zone.y + zone.height / 2 - CIRCLE_RADIUS;
@@ -80,59 +87,104 @@ export default class DragAndDropExample extends React.Component{
         Animated.spring(this.state.pan, { toValue: { ...springTo } }).start();
       }
     });
-  }
+  };
 
   updateDotLayout = (event, dotNumber) => {
-    const dotsLayout = this.state.dotsLayout;
+    const dotsLayout = this.dotsLayout;
     dotsLayout[dotNumber] = event.nativeEvent.layout;
     this.setState({ dotsLayout });
-  }
+  };
 
-  renderDots = (n) => {
+  renderDots = n => {
     const dots = [];
     for (let i = 0; i < n; i++) {
       dots.push(
         <View
           key={`answer_${i}`}
-          onLayout={(e) => this.updateDotLayout(e, i)}
+          onLayout={e => this.updateDotLayout(e, i)}
           style={{
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
-            minWidth: '25%',
+            minWidth: '25%'
           }}
         >
           <View style={styles.dot} />
         </View>
-      )
+      );
     }
     return (
       <View
         style={{
           height: '25%',
           flexDirection: 'row',
-          flexWrap: 'wrap',
+          flexWrap: 'wrap'
         }}
       >
         {dots}
       </View>
     );
-  }
+  };
 
-  renderDraggable = () => (
-    <View style={{ position: 'absolute' }}>
-      <Animated.View
-        {...this.panResponder.panHandlers}
-        style={[this.state.pan.getLayout(), styles.circle]}
-      >
-        <Text style={styles.text}>Drag me!</Text>
-      </Animated.View>
-    </View>
-  )
+  updateWordLayout = (event, index) => {
+    const layout = event.nativeEvent.layout;
+    this.initialPosition = { x: layout.x, y: layout.y };
+    this.state.pan = new Animated.ValueXY(this.initialPosition);
+    this.initPanResponder();
+    this.setState({ pan: this.state.pan });
+  };
+
+  renderDraggable = () => {
+    // the idea is to place objects first on the screen using flex divs,
+    // and only then make it interactable and init the coordinates
+    let containerStyle = { position: 'absolute', backgroundColor: 'orange' };
+    let itemStyle, itemPanHandlers, itemOnLayout;
+    if (this.state.pan === null) {
+      containerStyle = {
+        ...containerStyle,
+        width: '100%',
+        paddingTop: Window.height / 2,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+      };
+      itemStyle = [styles.circle];
+      itemPanHandlers = {};
+      itemOnLayout = e => this.updateWordLayout(e, 0);
+    } else {
+      itemStyle = [this.state.pan.getLayout(), styles.circle];
+      itemPanHandlers = this.panResponder.panHandlers;
+      itemOnLayout = () => {};
+    }
+    return (
+      <View style={containerStyle}>
+        <View style={{ backgroundColor: 'white' }}><Text>test</Text></View>
+        <Animated.View
+          {...itemPanHandlers}
+          style={itemStyle}
+          onLayout={itemOnLayout}
+        >
+          <Text style={styles.text}>Drag me!</Text>
+        </Animated.View>
+        <View style={{ width: 100, height: 100, backgroundColor: 'white' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'white' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'white' }} />
+      </View>
+    );
+    // <View style={{ position: 'absolute', backgroundColor: 'orange' }}>
+    <Animated.View
+      {...this.panResponder.panHandlers}
+      style={[this.state.pan.getLayout(), styles.circle]}
+    >
+      <Text style={styles.text}>Drag me!</Text>
+    </Animated.View>;
+    // </View>
+  };
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}>
         {this.renderDots(6)}
         {this.renderDraggable()}
       </View>
