a1691d1050b3fc7585698ec08bd304af18a0e6e9
Igor Zamyslov <igor.zamyslov@cognostics.de>
2017-06-12 09:31:39 +0200

    Moved component to exercise folder, changed styles to match overall style of the app. Now branch is based on master branch of serlo-abc.

---
+++ src/components/exercises/BuildSentence.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+
+import BuildSentence from './BuildSentence';
+
+storiesOf('exercises/BuildSentence', module).add('example', () =>
+  <BuildSentence
+    sentence={[
+      'JavaScript',
+      'React',
+      'using',
+      'mobile',
+      'Build',
+      'apps',
+      'native',
+      'and'
+    ]}
+  />
+);
