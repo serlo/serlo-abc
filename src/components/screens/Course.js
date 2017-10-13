bd9479734a00dea9a23e079c1127d4e537fa4bf0
Jonas Keinholz <jonas@keinholz.com>
2017-10-13 11:15:28 +0200

    screens/Course: only show UI component after course has loaded

--- src/components/screens/Course.js
+++ src/components/screens/Course.js
@@ -12,20 +12,35 @@ class Course extends Component {
     const storage = new Storage(courses);
     const progress = null; // TODO:
     this.interactor = new Interactor(storage, progress);
+
+    this.state = {
+      course: null
+    };
   }
 
   componentDidMount() {
     this.interactor
       .loadCourse('09438926-b170-4005-a6e8-5dd8fba83cde')
       .then(() => {
-        console.warn('loaded');
+        this.interactor.getStructure(course => {
+          this.setState({ course });
+        });
       });
   }
 
   render() {
+    const { course } = this.state;
+
+    if (!course) {
+      return null;
+    }
+
+    console.warn(course);
+
+    // TODO: move UI component out of here.
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>This is a course</Text>
+        <Text>This is the course with id {course.id}</Text>
       </View>
     );
   }
