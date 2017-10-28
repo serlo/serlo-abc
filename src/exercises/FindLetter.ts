8c0c3dfcf6162061e7a33ec9fca07cf22ce2a014
Jonas Keinholz <jonas@keinholz.com>
2017-10-28 17:24:26 +0200

    feat(exercises): FindLetter (#154)

---
+++ src/exercises/FindLetter.ts
@@ -0,0 +1,32 @@
+import { map } from 'ramda';
+
+import { getWord } from '../helpers/words.js';
+import AbstractExercise from './AbstractExercise';
+
+interface IProps {
+  word: any;
+  letter: string;
+}
+
+type IState = boolean[];
+
+class FindLetter extends AbstractExercise<IProps, IState> {
+  public getInitialState() {
+    return map(() => false, getWord(this.getProps().word)) as IState;
+  }
+
+  public isCorrect(state: IState) {
+    const { letter, word } = this.getProps();
+    const wordString = getWord(word).toUpperCase();
+
+    for (let i = 0; i < wordString.length; i++) {
+      if (state[i] !== (wordString[i] === letter)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+export default FindLetter;
