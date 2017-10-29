5fdcd7a00ee40204235d1b0b5bb973647cf65aa0
Knorrke <knorr.b@gmx.de>
2017-10-29 20:04:37 +0100

    merge master into local branch

--- src/exercises/index.ts
+++ src/exercises/index.ts
@@ -2,13 +2,14 @@ import * as ChooseArticle from './ChooseArticle';
 import * as DifferFromSymbol from './DifferFromSymbol';
 import * as FindLetter from './FindLetter';
 import * as HearWord from './HearWord';
+import * as InfoScreen from './InfoScreen';
 import * as LetterRotated from './LetterRotated';
 import * as MatchImage from './MatchImage';
 import * as MissingText from './MissingText';
 import * as VideoQuestion from './VideoQuestion';
-
 export {
   FindLetter,
+  InfoScreen,
   ChooseArticle,
   DifferFromSymbol,
   HearWord,
