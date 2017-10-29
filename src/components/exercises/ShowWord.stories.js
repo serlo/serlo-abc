0b2b46f1330450aa75a56355a8e501fb6a8fe7f0
Jonas Keinholz <jonas@keinholz.com>
2017-10-29 20:05:47 +0100

    feat(exercises): ShowWord

--- src/components/exercises/ShowWord.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-
-import { getWordObject } from '../../helpers/words';
-import ShowWord from './ShowWord';
-
-storiesOf('exercises/ShowWord', module)
-  .add('Ananas', () => <ShowWord word={getWordObject('ananas')} letter="A" />)
-  .add('Affe', () => <ShowWord word={getWordObject('affe')} letter="A" />);
