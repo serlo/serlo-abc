519ea0c52806f6e675b125524cec994323eba93d
Jonas Keinholz <jonas@keinholz.com>
2017-11-06 06:32:17 +0100

    feat(courses.json): section Hallo!

--- src/assets/videos.ts
+++ src/assets/videos.ts
@@ -4,7 +4,19 @@ const videos: { [id: string]: () => IVideoAsset } = {
   explanation_show_letter: () =>
     require('./videos/explanation_show_letter.mp4'),
   explanation_show_word: () => require('./videos/explanation_show_word.mp4'),
-  praise1: () => require('./videos/praise1.mp4')
+  praise1: () => require('./videos/praise1.mp4'),
+  praise2: () => require('./videos/praise2.mp4'),
+  praise3: () => require('./videos/praise3.mp4'),
+  praise4: () => require('./videos/praise4.mp4'),
+  praise5: () => require('./videos/praise5.mp4'),
+  praise6: () => require('./videos/praise6.mp4'),
+  praise7: () => require('./videos/praise7.mp4'),
+  praise8: () => require('./videos/praise8.mp4'),
+  praise9: () => require('./videos/praise9.mp4'),
+  praise10: () => require('./videos/praise10.mp4'),
+  praise11: () => require('./videos/praise11.mp4'),
+  praise12: () => require('./videos/praise12.mp4'),
+  video_ane: () => require('./videos/video_ane.mp4')
 };
 
 export default videos;
