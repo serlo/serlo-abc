76b60815b71b4b4664bccb1f9b2adea7956799c3
Knorrke <knorr.b@gmx.de>
2017-11-13 15:08:06 +0100

    exercises: add feedback to LetterRotated

--- packages/entities/exercises/LetterRotated/LetterRotated.ts
+++ packages/entities/exercises/LetterRotated/LetterRotated.ts
@@ -1,5 +1,7 @@
-import { all, contains, map } from 'ramda';
+import { addIndex, all, contains, map } from 'ramda';
 import AbstractExercise from '../AbstractExercise';
+
+const mapIndexed = addIndex(map);
 
 export interface IProps {
   letters: string[];
@@ -10,9 +12,35 @@ export interface IProps {
 
 export type IState = boolean[];
 
-class LetterRotated extends AbstractExercise<IProps, IState> {
+export interface IFeedback {
+  wrongChoices?: boolean[];
+  correctChoice?: boolean[];
+  missingCorrectChoices?: boolean[];
+}
+
+class LetterRotated extends AbstractExercise<IProps, IState, IFeedback> {
   public getInitialState() {
     return map(() => false, this.props.letters) as IState;
+  }
+
+  public getFeedback(selected: IState) {
+    if (this.isSubmitDisabled(selected) || this.isCorrect(selected)) {
+      return {};
+    }
+    return {
+      wrongChoices: mapIndexed(
+        (selection, index) => selection && !contains(index, this.props.rotated),
+        selected
+      ),
+      correctChoices: mapIndexed(
+        (selection, index) => selection && contains(index, this.props.rotated),
+        selected
+      ),
+      missingCorrectChoices: mapIndexed(
+        (selection, index) => !selection && contains(index, this.props.rotated),
+        selected
+      )
+    };
   }
 
   public isCorrect(selected: IState) {
