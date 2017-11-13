7d5f8260387883e1b10f6aaff278c2c4cb9ddfa1
Knorrke <knorr.b@gmx.de>
2017-11-13 11:27:41 +0100

    exercises: add feedback to InfoScreens

--- packages/entities/exercises/InfoScreenWithSounds/InfoScreenWithSounds.ts
+++ packages/entities/exercises/InfoScreenWithSounds/InfoScreenWithSounds.ts
@@ -2,11 +2,21 @@ import AbstractExercise from '../AbstractExercise';
 
 export type IState = boolean;
 
-class InfoScreenWithSounds<Props> extends AbstractExercise<Props, IState> {
+export type IFeedback = void;
+
+class InfoScreenWithSounds<Props> extends AbstractExercise<
+  Props,
+  IState,
+  IFeedback
+> {
   public initiallyCorrect = true;
 
   public getInitialState() {
     return false;
+  }
+
+  public getFeedback() {
+    return;
   }
 
   public isCorrect(state: IState) {
