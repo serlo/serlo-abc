4fe10b33f302686c18d1cdb153bd2b3ec82e82cf
Knorrke <knorr.b@gmx.de>
2017-11-13 11:02:12 +0100

    feat(exercises/DifferFromSymbol): add feedback

--- packages/entities/exercises/DifferFromSymbol/DifferFromSymbol.ts
+++ packages/entities/exercises/DifferFromSymbol/DifferFromSymbol.ts
@@ -8,9 +8,23 @@ export interface IProps {
 
 export type IState = Optional<number>;
 
-class DifferFromSymbol extends AbstractExercise<IProps, IState> {
+export interface IFeedback {
+  highlightedChoice?: number;
+}
+
+class DifferFromSymbol extends AbstractExercise<IProps, IState, IFeedback> {
   public getInitialState() {
     return undefined;
+  }
+
+  public getFeedback(selectedIndex: IState) {
+    if (this.isSubmitDisabled(selectedIndex) || this.isCorrect(selectedIndex)) {
+      return {};
+    }
+
+    return {
+      highlightedChoice: selectedIndex
+    };
   }
 
   public isCorrect(selectedIndex: IState) {
