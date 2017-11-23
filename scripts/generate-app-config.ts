91e9b95880132d2ecc875e4d844439803a1e7c08
Jonas Keinholz <jonas@keinholz.com>
2017-11-23 20:39:15 +0100

    build: upgrade to Expo SDK 23
    * Remove @types/react from package.json (since @types/react-native already already has a working one as dependency)
    * Enable [`ios.loadJSInBackgroundExperimental`](https://docs.expo.io/versions/v23.0.0/guides/configuration.html#loadjsinbackgroundexperimental) for faster start up time (and behaviour consistent with Android version)
    * Switch to [Expo's fork of React Native](https://docs.expo.io/versions/v23.0.0/introduction/faq.html#why-does-expo-use-a-fork-of-react-native)
    * Constrain drawable area of `TextCanvas`
    * Remove `Canvas` story (since the component does not work as intended without its wrapper components)
    * Use [Release Channels](https://docs.expo.io/versions/v23.0.0/guides/release-channels.html).
      * Branch `master` will be deployed to `default` release channel
      * Branch `development` will be deployed to `development` release channel

--- scripts/generate-app-config.ts
+++ scripts/generate-app-config.ts
@@ -1,31 +1,23 @@
+/* tslint:disable:no-var-requires */
 import * as fs from 'fs';
 import * as path from 'path';
-import { identity, mergeDeepRight } from 'ramda';
+import { mergeDeepRight } from 'ramda';
 
-export interface Config {
-  /* tslint:disable-next-line:no-any */
-  [propName: string]: any;
-}
+const config = require('../app.json');
+const { version } = require('../package.json');
 
-export const writeTravisAppConfigSync = (
-  override: (baseConfig: Config) => Config = identity
-) => {
-  const config = require('../app.json');
-  const { version } = require('../package.json');
-
-  const baseConfig = mergeDeepRight(config, {
-    expo: {
-      version,
-      packagerOpts: {
-        nonPersistent: true
-      }
+const baseConfig = mergeDeepRight(config, {
+  expo: {
+    version,
+    packagerOpts: {
+      nonPersistent: true
     }
-  });
+  }
+});
 
-  const travisConfig = override(baseConfig);
+const travisConfig = baseConfig;
 
-  fs.writeFileSync(
-    path.join(__dirname, '..', 'app.json'),
-    JSON.stringify(travisConfig, undefined, 2)
-  );
-};
+fs.writeFileSync(
+  path.join(__dirname, '..', 'app.json'),
+  JSON.stringify(travisConfig, undefined, 2)
+);
