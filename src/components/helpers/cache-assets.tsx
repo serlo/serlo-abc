c6d376dd6185da74300a2e322363d80f47670031
Jonas Keinholz <jonas@keinholz.com>
2017-12-01 06:03:21 +0100

    feat: preload and cache assets

---
+++ src/components/helpers/cache-assets.tsx
@@ -0,0 +1,54 @@
+/* tslint:disable:no-any */
+// @ts-ignore: TODO: declaration file
+import { Asset, Font } from 'expo';
+import { map } from 'ramda';
+import * as React from 'react';
+
+interface CacheAssetsProps {
+  fonts?: any[];
+  assets?: any[];
+  render: (
+    done: boolean
+  ) =>
+    | JSX.Element
+    | JSX.Element[]
+    | React.ReactPortal
+    | string
+    | number
+    | null
+    | false;
+}
+
+interface CacheAssetsState {
+  done: boolean;
+}
+
+export class CacheAssets extends React.Component<
+  CacheAssetsProps,
+  CacheAssetsState
+> {
+  public state = {
+    done: false
+  };
+
+  public componentDidMount() {
+    const fonts = this.cacheFonts(this.props.fonts);
+    const assets = this.cacheAssets(this.props.assets);
+
+    Promise.all([...fonts, ...assets]).then(() => {
+      this.setState({ done: true });
+    });
+  }
+
+  public render() {
+    return this.props.render(this.state.done);
+  }
+
+  private cacheFonts(fonts: {} = {}): Array<Promise<void>> {
+    return Font.loadAsync(fonts);
+  }
+
+  private cacheAssets(assets: any[] = []): Array<Promise<void>> {
+    return map(asset => Asset.fromModule(asset).downloadAsync(), assets);
+  }
+}
