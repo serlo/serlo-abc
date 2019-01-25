4c3f44bb6f71357c26b8e1481e86ee7f21a145be
Jonas Keinholz <jonas@keinholz.com>
2019-01-25 14:24:45 +0100

    refactor: generate sdkVersion from package.json

--- scripts/generate-app-config.ts
+++ scripts/generate-app-config.ts
@@ -1,17 +1,21 @@
 /* tslint:disable:no-var-requires */
 import * as fs from 'fs';
 import * as path from 'path';
+import * as semver from 'semver';
 import { mergeDeepRight } from 'ramda';
 
 const config = require('../app.json');
-const { version, versionCode } = require('../package.json');
+const { version, versionCode, dependencies } = require('../package.json');
 
 const isDev = process.argv[2] === 'development';
+const expoVersion = dependencies['expo'].replace('^', '');
+const sdkVersion = `${semver.major(expoVersion)}.0.0`;
 
 const travisConfig = mergeDeepRight(config, {
   expo: {
     name: `${config.expo.name}${isDev ? ' Nightly' : ''}`,
     slug: `${config.expo.slug}${isDev ? '-development' : ''}`,
+    sdkVersion,
     version,
     packagerOpts: {
       nonPersistent: true
